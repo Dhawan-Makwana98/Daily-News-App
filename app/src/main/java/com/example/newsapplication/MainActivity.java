fe416db85de0baca05d9558644daf59bd5ee94fc
Dhawan-Makwana98 <dhavanmak@gmail.com>
2024-08-19 17:06:07 +0530

    Bug Fixes for URL crashes

--- app/src/main/java/com/example/newsapplication/MainActivity.java
+++ app/src/main/java/com/example/newsapplication/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -35,26 +34,30 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
     private NewsRVAdapter newsRVAdapter;
     private CategoryRVAdapter categoryRVAdapter;
 
-    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize views and data
         newsRV = findViewById(R.id.idRVNews);
         categoryRV = findViewById(R.id.idRVCategories);
         loadingPB = findViewById(R.id.idPBLoading);
+
         articlesArrayList = new ArrayList<>();
         categoryRVModalsArrayList = new ArrayList<>();
+
+        // Set up RecyclerView and Adapters
         newsRVAdapter = new NewsRVAdapter(articlesArrayList, this);
         categoryRVAdapter = new CategoryRVAdapter(categoryRVModalsArrayList, this, this::onDeveloperClick);
+
         newsRV.setLayoutManager(new LinearLayoutManager(this));
         newsRV.setAdapter(newsRVAdapter);
         categoryRV.setAdapter(categoryRVAdapter);
+
+        // Load categories and news
         getCategory();
         getNews("All");
-
-        newsRVAdapter.notifyDataSetChanged();
     }
 
     @SuppressLint("NotifyDataSetChanged")
@@ -74,54 +77,43 @@ public class MainActivity extends AppCompatActivity implements CategoryRVAdapter
     private void getNews(String category) {
         loadingPB.setVisibility(View.VISIBLE);
         articlesArrayList.clear();
-        String categoryURL = "https://newsapi.org/v2/top-headlines?country=in&category=" + category + "&apiKey=109358310bf14ce5b0ec3cf60aa41a61";
-        String url = "https://newsapi.org/v2/everything?q=india&from=2024-07-15&sortBy=publishedAt&apiKey=109358310bf14ce5b0ec3cf60aa41a61";
+
         String BASE_URL = "https://newsapi.org/";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        RetrofitAPI retrofit1 = retrofit.create(RetrofitAPI.class);
-        com.example.newsapp.RetrofitAPI retrofitAPI = retrofit.create(com.example.newsapp.RetrofitAPI.class);
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
 
-        Call<NewsModal> call;
-        call = retrofitAPI.getAllNews(categoryURL);
+        String apiKey = "109358310bf14ce5b0ec3cf60aa41a61";
+        String categoryURL = "https://newsapi.org/v2/top-headlines?country=in&category=" + category + "&apiKey=" + apiKey;
+        //String url = "https://newsapi.org/v2/everything?q=india&from=2024-07-15&sortBy=publishedAt&apiKey=" + apiKey;
+        String url = "https://newsapi.org/v2/top-headlines?mumbai=us&category=technology&apiKey=" + apiKey;
 
-        if (category.equals("All")){
-            call= retrofitAPI.getAllNews(url);
-        }
-        else{
-            call= retrofitAPI.getNewsByCategory(categoryURL);
-        }
+        Call<NewsModal> call = category.equals("All") ? retrofitAPI.getAllNews(url) : retrofitAPI.getNewsByCategory(categoryURL);
 
         call.enqueue(new Callback<NewsModal>() {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onResponse(@NonNull Call<NewsModal> call, @NonNull Response<NewsModal> response) {
-                NewsModal newsModal = response.body();
                 loadingPB.setVisibility(View.GONE);
-                assert newsModal != null;
-                ArrayList<Articles> articles = newsModal.getArticles();
-                for (int i = 0; i < articles.size(); i++) {
-                    articlesArrayList.add(new Articles(
-                            articles.get(i).getTitle(),
-                            articles.get(i).getDescription(),
-                            articles.get(i).getUrlToImage(),
-                            articles.get(i).getUrl(),
-                            articles.get(i).getContent()
-                    ));
+                if (response.body() != null) {
+                    ArrayList<Articles> articles = response.body().getArticles();
+                    if (articles != null) {
+                        articlesArrayList.addAll(articles);
+                    }
+                    newsRVAdapter.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(MainActivity.this, "No news available", Toast.LENGTH_SHORT).show();
                 }
-
-                newsRVAdapter.notifyDataSetChanged();
-
-             }
+            }
 
             @Override
-            public void onFailure(Call<NewsModal> call, Throwable t) {
-                Toast.makeText(MainActivity.this, "Fail to get news", Toast.LENGTH_SHORT).show();
+            public void onFailure(@NonNull Call<NewsModal> call, @NonNull Throwable t) {
+                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Failed to get news", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 
     @Override
